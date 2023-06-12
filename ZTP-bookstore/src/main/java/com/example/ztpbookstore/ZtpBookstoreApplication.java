b9bba3d99f08e4f83250fecd948f53b4fa0a038b
Kazan33333 <kazan.kk@gmail.com>
2023-06-12 15:50:09 +0200

    sth ain't workin'

---
+++ ZTP-bookstore/src/main/java/com/example/ztpbookstore/ZtpBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ztpbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtpBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtpBookstoreApplication.class, args);
+	}
+
+}
---
+++ ZTP-bookstore/src/test/java/com/example/ztpbookstore/ZtpBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ztpbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtpBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
